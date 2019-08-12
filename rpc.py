759c8484406f2e7da6ff004b533e191568a0ba42
Jacks <52548283+JacksMuch@users.noreply.github.com>
2019-08-11 21:49:33 -0400

    Added details

--- rpc.py
+++ rpc.py
@@ -4,6 +4,6 @@ import time
 client_id = "610284471765303306"
 RPC = Presence(client_id)
 RPC.connect()
-RPC.update(state="https://discord.boats", large_image="boats")
+RPC.update(state="https://discord.boats", details="Website", large_image="boats")
 while True:
     time.sleep(15)
