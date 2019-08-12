8e05a88237df987c6e59c2ca811f24d17f5031d7
Jacks <52548283+JacksMuch@users.noreply.github.com>
2019-08-11 21:40:00 -0400

    Add files via upload

---
+++ rpc.py
@@ -0,0 +1,9 @@
+from pypresence import Presence
+import time
+
+client_id = "610284471765303306"
+RPC = Presence(client_id)
+RPC.connect()
+RPC.update(state="https://discord.boats")
+while True:
+    time.sleep(15)
